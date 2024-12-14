dfec379b2e49721bd9e7e2ded9a8b2d026d01051
Rushikesh Patil <rushikesh.patil@nulou.in>
2024-12-14 22:37:23 +0530

    add env file on frontend folder

--- frontEnd/src/socket.js
+++ frontEnd/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:8000'; // Replace with your backend URL
+const SOCKET_URL = import.meta.env.VITE_APP_SOKET_URL; // Replace with your backend URL
 const socket = io(SOCKET_URL, {
     autoConnect: false, // Connect manually after initialization
 });
